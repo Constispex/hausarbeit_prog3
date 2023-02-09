5501b8cf49b4141b8535dc0e44eb03f697de4982
Constantin <cofritzsch@gmail.com>
2023-02-09 12:40:27 +0100

    endlich fehler gefunden

--- Common/src/main/java/de/prog3/common/User.java
+++ Common/src/main/java/de/prog3/common/User.java
@@ -1,12 +1,18 @@
 package de.prog3.common;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Speichert Name und Passwort und Adminrechte von einem Benutzer.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
-
+    @JsonProperty("name")
     public String name;
+    @JsonProperty("password")
     public String password;
+    @JsonProperty("isAdmin")
     public boolean isAdmin;
 
     public User() {
@@ -37,5 +43,24 @@ public class User {
         return password;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", isAdmin=" + isAdmin +
+                '}';
+    }
 }
